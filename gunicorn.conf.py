cd639c079e24bd06dd00e6d7494cadf342e06ec0
Prince Kumar <princekumar123pr17@gmail.com>
2026-04-02 12:47:40 +0530

    Reduce Gunicorn workers for Railway

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,8 @@
-import multiprocessing
 import os
 
 
 bind = os.getenv("GUNICORN_BIND", f"0.0.0.0:{os.getenv('PORT', '8000')}")
-workers = int(os.getenv("GUNICORN_WORKERS", max(3, multiprocessing.cpu_count() * 2 - 1)))
+workers = int(os.getenv("GUNICORN_WORKERS", os.getenv("WEB_CONCURRENCY", "1")))
 threads = int(os.getenv("GUNICORN_THREADS", "2"))
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
 graceful_timeout = int(os.getenv("GUNICORN_GRACEFUL_TIMEOUT", "30"))
